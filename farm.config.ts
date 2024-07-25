ee785fe8a6657e7bce6f09f1373f9cda4105cb54
Kenji Saito <ken-yo@mbr.nifty.com>
2024-07-25 21:55:46 +0900

    Add the example app

--- farm.config.ts
+++ farm.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from '@farmfe/core';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  compilation: {
+    persistentCache: true,
+  },
   vitePlugins: [
     () => ({
+      name: 'solid',
       vitePlugin: solid(),
       filters: ['\\.tsx$', '\\.jsx$'],
     }),
