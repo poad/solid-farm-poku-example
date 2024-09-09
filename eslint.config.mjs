53b4110215108eb572adeb43456164c559e06c9e
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-09 21:31:06 +0900

    Bumps node modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,16 @@ import stylistic from '@stylistic/eslint-plugin';
 import stylisticTs from '@stylistic/eslint-plugin-ts';
 import stylisticJsx from '@stylistic/eslint-plugin-jsx';
 import tseslint from 'typescript-eslint';
-import eslintImport from 'eslint-plugin-import';
+// @ts-expect-error ignore type errors
+import importPlugin from 'eslint-plugin-import';
+import pluginPromise from 'eslint-plugin-promise';
 import solid from 'eslint-plugin-solid';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const compat = new FlatCompat();
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  pluginPromise.configs['flat/recommended'],  
   {
     ignores: [
       '**/*.d.ts',
@@ -27,12 +28,29 @@ export default tseslint.config(
   },
   {
     files: ['src/*.{ts,tsx}', 'src/**/*.{ts,tsx}'],
-  },
-  {
+    ...importPlugin.flatConfigs.recommended,
+    ...importPlugin.flatConfigs.typescript,
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
     plugins: {
       '@stylistic': stylistic,
       '@stylistic/ts': stylisticTs,
       '@stylistic/jsx': stylisticJsx,
+      solid,
+    },
+    settings: {
+      'import/parsers': {
+        espree: ['.js', '.cjs', '.mjs'],
+        '@typescript-eslint/parser': ['.ts'],
+      },
+      'import/internal-regex': '^~/',
+      'import/resolver': {
+        node: true,
+        typescript: true,
+      },
     },
     rules: {
       '@stylistic/semi': 'error',
@@ -40,42 +58,6 @@ export default tseslint.config(
       '@stylistic/jsx/jsx-indent': ['error', 2],
       'comma-dangle': ['error', 'always-multiline'],
       'quotes': ['error', 'single'],
-    },
-  },
-  {
-    plugins: {
-      import: eslintImport,
-      solid,
-    },
-    extends: [
-      ...tseslint.configs.recommended,
-      ...compat.config(eslintImport.configs.recommended),
-      ...compat.config(eslintImport.configs.typescript),
-    ],
-    settings: {
-      'import/internal-regex': '^~/',
-      'import/resolver': {
-        node: {
-          extensions: ['.ts', '.tsx'],
-        },
-        typescript: {
-          alwaysTryTypes: true,
-        },
-      },
-    },
-    rules: {
-      'import/namespace': 'off',
-      'import/named': 'off',
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-unresolved': 'off',
-      'import/no-duplicates': 'off',
-      'import/default': 'off',
-      'import/export': 'off',
-    },
-  },
-  {
-    rules: {
       semi: ['error', 'always'],
     },
   },
--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,7 @@ import App from './App';
 
 const root = document.getElementById('root');
 
-render(() => <App />, root!);
+if (root) {
+  render(() => <App />, root);
+}
+
